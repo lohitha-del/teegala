c87abbfb3ee3e6254bc7bf7f6872cee50685c3d3
lohitha-del <22R91A1284@tkrec.ac.in>
2025-09-15 19:52:59 +0530

    datatypes

---
+++ teegala/src/com/tnsif/first/data.java
@@ -0,0 +1,26 @@
+package com.tnsif.first;
+
+public class data {
+
+	public static void main(String[] args) {
+		byte b=1;
+		short s =10;
+		int i =100;
+		long l = 1000000L;
+		float f = 3.14f;
+		 double d = 3.14159265359;
+		 char c = 'A'; 
+		 boolean flag = true;
+		System.out.println("byte"+b); 
+		System.out.println("short"+s);
+		System.out.println("int"+i);
+		System.out.println("long"+l);
+		System.out.println("float"+f);
+		System.out.println("double"+d);
+		System.out.println("char"+c);
+		System.out.println("boolean"+flag);
+		
+
+	}
+
+}

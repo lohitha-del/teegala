bd15871f1e76feafa904bfaeaea1c0a5df74fea1
lohitha-del <22R91A1284@tkrec.ac.in>
2025-10-09 18:05:28 +0530

    lamda

---
+++ teegala/src/com/tnsif/lamda/Annotations.java
@@ -0,0 +1,15 @@
+package com.tnsif.lamda;
+
+@FunctionalInterface
+interface Aaa{
+	public void show();
+	
+}
+public class Annotations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
